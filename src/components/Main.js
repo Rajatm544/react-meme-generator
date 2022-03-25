caf85a8076c3d1f73d4c16d7de291d338aef354b
Rajatm544 <rajatm544@gmail.com>
2022-03-26 00:04:14 +0530

    add all components to a parent component

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+import MemeDiv from './MemeDiv';
+import Footer from './Footer';
+
+const Main = () => {
+	return (
+		<>
+			<Navbar />
+			<MemeDiv />
+			<Footer />
+		</>
+	);
+};
+
+export default Main;
