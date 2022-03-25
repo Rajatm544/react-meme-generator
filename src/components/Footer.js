4a8ec20f12c1ad77791be3fd9eb94612b76ca204
Rajatm544 <rajatm544@gmail.com>
2022-03-26 00:03:31 +0530

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../stylesheets/footer.css';
+const Footer = () => {
+	return (
+		<footer>
+			Built By{' '}
+			<a
+				href='https://github.com/Rajatm544'
+				target='_blank'
+				rel='noreferrer noopener'>
+				Rajat
+			</a>
+		</footer>
+	);
+};
+
+export default Footer;
